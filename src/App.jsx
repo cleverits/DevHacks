96afd888d89a6e53476af018d48a1bcbc8c62833
cleverits <edmon2008sarg@gmail.com>
2025-06-19 19:25:30 +0400

    Fix routing For Github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,54 @@
-import './App.css'
-import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
-import NotFound from './NotFound';
-import Home from './Home/Home';
-import Posts from './Posts/Posts';
-import Blog from './Blog/Blog';
-import Contact from './Contact/Contact';
+import "./App.css";
+import { HashRouter as Router, Route, Routes, NavLink } from "react-router-dom";
+import NotFound from "./NotFound";
+import Home from "./Home/Home";
+import Posts from "./Posts/Posts";
+import Blog from "./Blog/Blog";
+import Contact from "./Contact/Contact";
 
 const links = [
   { link: "ԳԼԽԱՎՈՐ", route: "/", id: 1 },
   { link: "ԾԱՌԱՅՈՒԹՅՈՒՆՆԵՐ", route: "/posts", id: 2 },
   { link: "ԲԼՈԳ", route: "/blog", id: 3 },
-  { link: "ԿՈՆՏԱԿՏՆԵՐ", route: "/contact", id: 4 },   
+  { link: "ԿՈՆՏԱԿՏՆԵՐ", route: "/contact", id: 4 },
 ];
 
 export default function App() {
-    return (
-      <BrowserRouter>
-        <>
-          <header className="header">
-            <img
-              src={`${import.meta.env.BASE_URL}screenshot.png`}
-              alt="screenshot"
-              style={{
-                width: "200px",
-                height: "100px",
-                objectFit: "contain",
-                transform: "scale(1.1)",
-                objectPosition: "10% 100%",
-                cursor: "pointer",
-              }}
-            />
-            <nav className="navbar">
-              {links.map((item) => {
-                return (
-                  <NavLink to={item.route} key={item.id}>
-                    {item.link}
-                  </NavLink>
-                );
-              })}
-            </nav>
-          </header>
-        </>
+  return (
+    <Router>
+      <>
+        <header className="header">
+          <img
+            src={`${import.meta.env.BASE_URL}screenshot.png`}
+            alt="screenshot"
+            style={{
+              width: "200px",
+              height: "100px",
+              objectFit: "contain",
+              transform: "scale(1.1)",
+              objectPosition: "10% 100%",
+              cursor: "pointer",
+            }}
+          />
+          <nav className="navbar">
+            {links.map((item) => {
+              return (
+                <NavLink to={item.route} key={item.id}>
+                  {item.link}
+                </NavLink>
+              );
+            })}
+          </nav>
+        </header>
+      </>
 
-        <Routes>
-          <Route path="/" index element={<Home />} />
-          <Route path="/posts" element={<Posts />} />
-          <Route path="blog" element={<Blog />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    );
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
